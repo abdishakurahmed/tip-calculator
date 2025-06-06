8b4485c0f64e8b511665d30c474311534ebdf9dc
abdishakur ahmed <abdishakurahmed.2003@gmail.com>
2025-06-06 13:52:36 +0300

    select all the inputs, spans inside the the p element and the button

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Input elements
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const peopleInput = document.getElementById("people");
+
+// Button
+const calculateBtn = document.getElementById("calculate");
+
+// Output display spans
+const tipAmountDisplay = document.getElementById("tip-amount");
+const totalAmountDisplay = document.getElementById("total");
+const PerPersonDisplay = document.getElementById("per-person");
