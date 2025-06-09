d1a4343958d7caad45a3f473ae9f1d5498fa5f7f
abdishakur ahmed <abdishakurahmed.2003@gmail.com>
2025-06-09 18:22:57 +0300

    complete the function of tip calculator like calculating the values and checking on the values given. ther are still some things that finishing but most part are done

--- js/script.js
+++ js/script.js
@@ -11,9 +11,40 @@ const tipAmountDisplay = document.getElementById("tip-amount");
 const totalAmountDisplay = document.getElementById("total");
 const PerPersonDisplay = document.getElementById("per-person");
 
+// result div for adding paragraph to it if any errors occur
+const resultDiv = document.querySelector("#results");
+
 function tipCalculator() {
   // select the input values and stored in a variable
   const billInputValue = Number(billInput.value.trim());
-  const tipInputValue = Number(tipInput.value.trim());
+  const tipInputValue = Number(tipInput.value.trim()) / 100;
+  // console.log(tipInputValue);
   const peopleInputValue = Number(peopleInput.value.trim());
+
+  const para = document.createElement("p");
+  resultDiv.appendChild(para);
+
+  if (
+    Number.isNaN(billInputValue) ||
+    Number.isNaN(tipInputValue) ||
+    Number.isNaN(peopleInputValue)
+  ) {
+    para.textContent = "please enter a valid number";
+  } else if (
+    billInputValue <= 0 ||
+    tipInputValue <= 0 ||
+    peopleInputValue <= 0
+  ) {
+    para.textContent = "please enter a positive number";
+  } else {
+    tipAmountDisplay.textContent = `$${billInputValue * tipInputValue}`;
+    totalAmountDisplay.textContent = `$${
+      billInputValue * tipInputValue + billInputValue
+    }`;
+    PerPersonDisplay.textContent = `$${
+      (billInputValue * tipInputValue + billInputValue) / peopleInputValue
+    }`;
+  }
 }
+
+calculateBtn.addEventListener("click", tipCalculator);
